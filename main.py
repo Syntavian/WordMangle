d5b45cf49ce15b9b3db4a207922f69888ac03730
Thomas Sloman <syntavian@gmail.com>
2023-06-11 00:00:26 +1000

    build word segment map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-SEGMENT_LENGTH = 2
+SEGMENT_LENGTH = 3
+OVERLAP_LENGTH = 2
 
 inputs = ["doctor", "acrobat", "caravan", "police", "avalanche", "violence"]
 
@@ -12,18 +13,33 @@ def run():
 
     print(segments)
 
+    segment_map = generate_segment_map(segments)
+
+    print(segment_map)
+
     # TODO: Generate outputs
 
 
+def generate_segment_map(segments):
+    segment_map: dict[str, list[str]] = {}
+
+    for segment in segments:
+        if segment[:OVERLAP_LENGTH] in segment_map:
+            segment_map[segment[:OVERLAP_LENGTH]].append(segment)
+        else:
+            segment_map[segment[:OVERLAP_LENGTH]] = [segment]
+    return segment_map
+
+
 def generate_segments(_inputs):
-    segments = []
+    segments = set()
     for word in _inputs:
         for character_index in range(len(word)):
             segmentation_start = SEGMENT_LENGTH - 1
             if character_index >= segmentation_start:
                 segment_start = character_index - segmentation_start
                 segment_end = character_index + 1
-                segments.append(word[segment_start:segment_end])
+                segments.add(word[segment_start:segment_end])
     return segments
 
 
